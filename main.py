ba38442ca8da612526c433c150740984ed7f58e8
kbtanvir <tanvirkhaan004@gmail.com>
2024-05-17 00:13:43 +0600

    updated UI

--- main.py
+++ main.py
@@ -1,79 +1,97 @@
 import tkinter as tk
+from tkinter import ttk
 
 
 class PomodoroTimer:
-    def __init__(self, master):
+    def __init__(self, master: tk.Tk):
         self.master = master
 
-        master.title("Pomodoro Timer")
-        master.attributes("-topmost", True)
-        master.overrideredirect(True)
-        master.attributes("-toolwindow", True)
+        self.master.title("Pomodoro Timer")
+        self.master.geometry("320x200")
 
-        # Set the icon
-        icon_path = "app.ico"  # Replace this with the path to your icon
-        master.iconbitmap(icon_path)
+        self.master.attributes("-topmost", True)
 
         self.is_running = False
-        self.remaining_time = 0 * 3  # Initial time in seconds
+        self.is_fullscreen = False
+        self.remaining_time = 0
 
-        self.minutes_input = tk.Entry(master, width=5)
+        self.timer_display = tk.Label(self.master, text="", font=("Helvetica", 30))
+        self.timer_display.pack(pady=10)
+
+        self.controls_frame = tk.Frame(self.master)
+        self.controls_frame.pack(pady=10)
+
+        # ? INPUTS
+        # ? -------------------------------
+
+        self.minutes_label = tk.Label(self.controls_frame, text="Minutes:", font=("Helvetica", 12))
+        self.minutes_label.grid(row=0, column=0, padx=5)
+
+        self.minutes_input = tk.Entry(self.controls_frame, width=5, font=("Helvetica", 12))
         self.minutes_input.insert(0, "0")
-        self.minutes_input.pack(side=tk.LEFT)
+        self.minutes_input.grid(row=0, column=1, padx=5)
 
-        self.seconds_input = tk.Entry(master, width=5)
+        self.seconds_label = tk.Label(self.controls_frame, text="Seconds:", font=("Helvetica", 12))
+        self.seconds_label.grid(row=0, column=2, padx=5)
+
+        self.seconds_input = tk.Entry(self.controls_frame, width=5, font=("Helvetica", 12))
         self.seconds_input.insert(0, "3")
-        self.seconds_input.pack(side=tk.LEFT)
+        self.seconds_input.grid(row=0, column=3, padx=5)
 
-        self.start_button = tk.Button(master, text="▶", command=self.start_timer)
-        self.start_button.pack(side=tk.LEFT)
+        # ? BUTTONS
+        # ? -------------------------------
 
-        self.pause_button = tk.Button(master, text="⏸", command=self.pause_timer, state="disabled")
-        self.pause_button.pack(side=tk.LEFT)
+        self.start_button = ttk.Button(self.controls_frame, text="Start", command=self.start_timer)
+        self.start_button.grid(row=1, column=0, columnspan=1, padx=5, pady=5)
 
-        self.reset_button = tk.Button(master, text="🍤", command=self.reset_timer, state="disabled")
-        self.reset_button.pack(side=tk.LEFT)
+        self.pause_button = ttk.Button(self.controls_frame, text="Pause", command=self.pause_timer, state="disabled")
+        self.pause_button.grid(row=1, column=1, columnspan=1, padx=5, pady=5)
 
-        self.timer_display = tk.Label(master, text="", font=("Helvetica", 14))
-        self.timer_display.pack(side=tk.LEFT)
+        self.reset_button = ttk.Button(self.controls_frame, text="Reset", command=self.reset_timer, state="disabled")
+        self.reset_button.grid(row=1, column=2, columnspan=1, padx=5, pady=5)
 
-        self.break_label = tk.Label(master, text="Take Break", font=("Helvetica", 14))
-        self.break_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-        self.break_label.place_forget()
+        self.full_screen_button = ttk.Button(
+            self.controls_frame,
+            text="Full Screen",
+            command=self.toogle_fullscreen,
+            state="normal"
+        )
+        self.full_screen_button.grid(row=1, column=3, columnspan=1, padx=5, pady=5)
 
-        self.state_label = tk.Label(master, text="Timer", font=("Helvetica", 8))
-        # self.state_label.pack(side=tk.LEFT)
-
-        # Add minimize button
-        self.minimize_button = tk.Button(master, text="➖", command=self.minimize_window)
-        self.minimize_button.pack(side=tk.LEFT)
-
-        # Add close button
-        self.close_button = tk.Button(master, text="❌", command=self.close_window)
-        self.close_button.pack(side=tk.LEFT)
+        self.break_label = tk.Label(master, text="Take Break", font=("Helvetica", 30))
 
         self.update_timer_display()
 
-    def minimize_window(self):
-        self.master.iconify()
+    def toogle_fullscreen(self):
+        if self.is_fullscreen == True:
+            self.exit_fullscreen()
 
-    def close_window(self):
-        self.master.destroy()
+        else:
+            self.go_fullscreen()
 
-    def start_timer(self):
+    def exit_fullscreen(self):
+        # self.master.overrideredirect(True)
         self.master.attributes("-fullscreen", False)
         self.break_label.place_forget()
-        self.master.overrideredirect(True)
+        self.break_label.lift()
+        self.is_fullscreen = False
 
+    def go_fullscreen(self):
+        # self.master.overrideredirect(False)
+        self.timer_display.lower()
+        self.master.attributes("-fullscreen", True)
+        self.break_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+        self.is_fullscreen = True
+
+    def start_timer(self):
         if not self.is_running:
             minutes = int(self.minutes_input.get())
             seconds = int(self.seconds_input.get())
             self.remaining_time = minutes * 60 + seconds
             self.is_running = True
-            self.start_button.config(state="disabled")
-            self.pause_button.config(state="normal")
-            self.reset_button.config(state="normal")
-            self.update_state_label("Timer Running")
+            self.start_button["state"] = "disabled"
+            self.pause_button["state"] = "normal"
+            self.reset_button["state"] = "normal"
             self.run_timer()
 
     def run_timer(self):
@@ -84,37 +102,27 @@ class PomodoroTimer:
         time_format = '{:02d}:{:02d}'.format(mins, secs)
         self.timer_display.config(text=time_format)
         self.remaining_time -= 1
+        self.timer_id = self.master.after(1000, self.run_timer)
 
         if self.remaining_time == 0:
-            self.master.overrideredirect(False)
-            self.master.attributes("-fullscreen", True)
-            self.timer_display.lower()  # Lower timer_display
-            self.break_label.lift()  # Raise break_label
 
-        self.timer_id = self.master.after(1000, self.run_timer)
+            self.go_fullscreen()
 
     def pause_timer(self):
         if self.is_running:
             self.is_running = False
-            self.start_button.config(state="normal")
-            self.pause_button.config(state="disabled")
-            self.master.after_cancel(self.timer_id)  # Cancel the scheduled update
-            self.update_state_label("Timer Paused")
+            self.start_button["state"] = "normal"
+            self.pause_button["state"] = "disabled"
+            self.master.after_cancel(self.timer_id)
 
     def reset_timer(self):
         self.is_running = False
-        self.start_button.config(state="normal")
-        self.pause_button.config(state="disabled")
-        self.reset_button.config(state="disabled")
-        self.master.after_cancel(self.timer_id)  # Cancel the scheduled update
-        # self.master.attributes("-fullscreen", False)  # Exit full-screen mode
-        self.remaining_time = 25 * 60
+        self.start_button["state"] = "normal"
+        self.pause_button["state"] = "disabled"
+        self.reset_button["state"] = "disabled"
+        self.master.after_cancel(self.timer_id)
+        self.remaining_time = int(self.minutes_input.get()) * 60 + int(self.seconds_input.get())
         self.update_timer_display()
-        self.update_state_label("Timer Stopped")
-        self.break_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)  # Hide break_label
-
-    def update_state_label(self, state):
-        self.state_label.config(text=state)
 
     def update_timer_display(self):
         mins, secs = divmod(self.remaining_time, 60)
