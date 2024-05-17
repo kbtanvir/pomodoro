4c29f911eaf1e51de228f3685cf3767bd54b3b22
kbtanvir <tanvirkhaan004@gmail.com>
2024-05-17 12:18:15 +0600

    sticky feature added

--- main.py
+++ main.py
@@ -2,155 +2,178 @@ import tkinter as tk
 from tkinter import ttk
 
 
-class PomodoroTimer:
-    def __init__(self, master: tk.Tk):
-        self.master = master
-
-        self.master.title("Pomodoro Timer")
-        self.master.geometry("320x300")
-        self.master.attributes("-topmost", True)
-
+class AppState:
+    def __init__(self):
         self.is_running = False
         self.is_fullscreen = False
-        self.is_sticky = True
+        self.is_sticky = False
         self.remaining_time = 0
+        self.geometry = "320x180"
 
-        self.timer_display = tk.Label(self.master, text="", font=("Helvetica", 30))
-        self.timer_display.pack(pady=10)
 
-        self.controls_frame = tk.Frame(self.master)
-        self.controls_frame.pack(pady=10)
+class TimerDisplay(tk.Label):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
 
-        # ? INPUTS
-        # ? -------------------------------
+        self.config(font=("Helvetica", 40))
+        self.grid(row=0, column=1, padx=5)
 
-        self.minutes_label = tk.Label(self.controls_frame, text="Minutes:", font=("Helvetica", 12))
+
+class TimeInput(tk.Frame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.minutes_label = tk.Label(self, text="Minutes:", font=("Helvetica", 12))
         self.minutes_label.grid(row=0, column=0, padx=5)
 
-        self.minutes_input = tk.Entry(self.controls_frame, width=5, font=("Helvetica", 12))
+        self.minutes_input = tk.Entry(self, width=5, font=("Helvetica", 12), bd=2, relief="solid")
         self.minutes_input.insert(0, "0")
         self.minutes_input.grid(row=0, column=1, padx=5)
 
-        self.seconds_label = tk.Label(self.controls_frame, text="Seconds:", font=("Helvetica", 12))
+        self.seconds_label = tk.Label(self, text="Seconds:", font=("Helvetica", 12))
         self.seconds_label.grid(row=0, column=2, padx=5)
 
-        self.seconds_input = tk.Entry(self.controls_frame, width=5, font=("Helvetica", 12))
+        self.seconds_input = tk.Entry(self, width=5, font=("Helvetica", 12), bd=2, relief="solid")
         self.seconds_input.insert(0, "3")
-        self.seconds_input.grid(row=0, column=3, padx=5)
+        self.seconds_input.grid(row=0, column=3, padx=5, pady=5)
 
-        # ? BUTTONS
-        # ? -------------------------------
+        self.grid(row=1, column=1, padx=5)
 
-        self.start_button = ttk.Button(self.controls_frame, text="Start", command=self.start_timer)
-        self.start_button.grid(row=1, column=1, columnspan=1, padx=5, pady=5)
 
-        self.pause_button = ttk.Button(self.controls_frame, text="Pause", command=self.pause_timer, state="disabled")
-        self.pause_button.grid(row=1, column=2, columnspan=1, padx=5, pady=5)
+class App(tk.Tk):
+    def __init__(self, state, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.state: AppState = state
 
-        self.reset_button = ttk.Button(self.controls_frame, text="Reset", command=self.reset_timer, state="disabled")
-        self.reset_button.grid(row=2, column=1, columnspan=1, padx=5, pady=5)
+        self.title("Pomodoro Timer")
+        self.geometry(self.state.geometry)
+        self.attributes("-topmost", True)
 
-        self.full_screen_button = ttk.Button(
-            self.controls_frame,
-            text="Full Screen",
-            command=self.toggle_fullscreen,
-            state="normal"
-        )
-        self.full_screen_button.grid(row=2, column=2, columnspan=1, padx=5, pady=5)
+        self.controls_frame = tk.Frame(self)
+        self.controls_frame.pack(pady=0)
 
-        self.sticky = ttk.Button(
-            self.controls_frame,
-            text=f"Sticky:{self.is_sticky}",
-            command=self.toggle_sticky,
-            state="normal"
-        )
-        self.sticky.grid(row=3, column=1, columnspan=1, padx=5, pady=5)
+        self.timer_display = TimerDisplay(self.controls_frame, text="00:00")
 
-        self.break_label = tk.Label(master, text="Take Break", font=("Helvetica", 30))
+        self.time_input = TimeInput(self.controls_frame)
 
-        self.update_timer_display()
+        self.control_handler = CommandHandler(self)
 
-    def toggle_sticky(self):
+        self.control_buttons = ControlButtons(self.controls_frame, self.control_handler)
 
-        if self.is_sticky == True:
-            self.master.attributes("-topmost", True)
-            # self.master.overrideredirect(False)
+        self.timer_id = None
 
-        else:
-            self.master.geometry('100x50')
-            self.master.attributes("-topmost", False)
-            # self.master.overrideredirect(True)
+        self.bind("<Button-1>", self.toggle_sticky_on_click)
 
-        self.is_sticky = not self.is_sticky
-
-    def toggle_fullscreen(self):
-        self.exit_fullscreen() if self.is_fullscreen else self.go_fullscreen()
-
-    def exit_fullscreen(self):
-        # self.master.overrideredirect(True)
-        self.master.attributes("-fullscreen", False)
-        self.break_label.place_forget()
-        self.break_label.lift()
-        self.is_fullscreen = False
-
-    def go_fullscreen(self):
-        # self.master.overrideredirect(False)
-        self.timer_display.lower()
-        self.master.attributes("-fullscreen", True)
-        self.break_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-        self.is_fullscreen = True
-
-    def start_timer(self):
-        if not self.is_running:
-            minutes = int(self.minutes_input.get())
-            seconds = int(self.seconds_input.get())
-            self.remaining_time = minutes * 60 + seconds
-            self.is_running = True
-            self.start_button["state"] = "disabled"
-            self.pause_button["state"] = "normal"
-            self.reset_button["state"] = "normal"
-            self.run_timer()
+    def toggle_sticky_on_click(self, event):
+        if self.state.is_sticky:
+            self.control_handler.toggle_sticky()
 
     def run_timer(self):
-        if self.remaining_time <= 0:
-            self.reset_timer()
+        if self.state.remaining_time <= 0:
+            self.control_handler.reset_timer()
             return
-        mins, secs = divmod(self.remaining_time, 60)
+        mins, secs = divmod(self.state.remaining_time, 60)
         time_format = '{:02d}:{:02d}'.format(mins, secs)
         self.timer_display.config(text=time_format)
-        self.remaining_time -= 1
-        self.timer_id = self.master.after(1000, self.run_timer)
-
-        if self.remaining_time == 0:
-
-            self.go_fullscreen()
-
-    def pause_timer(self):
-        if self.is_running:
-            self.is_running = False
-            self.start_button["state"] = "normal"
-            self.pause_button["state"] = "disabled"
-            self.master.after_cancel(self.timer_id)
-
-    def reset_timer(self):
-        self.is_running = False
-        self.start_button["state"] = "normal"
-        self.pause_button["state"] = "disabled"
-        self.reset_button["state"] = "disabled"
-        self.master.after_cancel(self.timer_id)
-        self.remaining_time = int(self.minutes_input.get()) * 60 + int(self.seconds_input.get())
-        self.update_timer_display()
+        self.state.remaining_time -= 1
+        self.timer_id = self.after(1000, self.run_timer)
+        if self.state.remaining_time == 0:
+            self.control_handler.toggle_fullscreen()
 
     def update_timer_display(self):
-        mins, secs = divmod(self.remaining_time, 60)
+        mins, secs = divmod(self.state.remaining_time, 60)
         time_format = '{:02d}:{:02d}'.format(mins, secs)
         self.timer_display.config(text=time_format)
+
+
+class CommandHandler:
+    def __init__(self, app: App):
+        self.app = app
+
+    def start_timer(self):
+        if not self.app.state.is_running:
+            if self.app.state.remaining_time == 0:
+                # If no remaining time is stored, initialize from input fields
+                minutes = int(self.app.time_input.minutes_input.get())
+                seconds = int(self.app.time_input.seconds_input.get())
+                self.app.state.remaining_time = minutes * 60 + seconds
+            self.app.state.is_running = True
+            self.app.control_buttons.start_button.config(state="disabled")
+            self.app.control_buttons.pause_button.config(state="normal")
+            self.app.control_buttons.reset_button.config(state="normal")
+            self.app.run_timer()
+
+    def pause_timer(self):
+        if self.app.state.is_running:
+            self.app.state.is_running = False
+            self.app.control_buttons.start_button.config(state="normal")
+            self.app.control_buttons.pause_button.config(state="disabled")
+            self.app.after_cancel(self.app.timer_id)
+
+    def reset_timer(self):
+        self.app.state.is_running = False
+        self.app.control_buttons.start_button.config(state="normal")
+        self.app.control_buttons.pause_button.config(state="disabled")
+        self.app.control_buttons.reset_button.config(state="disabled")
+        self.app.after_cancel(self.app.timer_id)
+        self.app.state.remaining_time = int(self.app.time_input.minutes_input.get()) * 60 + int(
+            self.app.time_input.seconds_input.get())
+        self.app.update_timer_display()
+
+    def toggle_fullscreen(self):
+        if not self.app.state.is_fullscreen:
+            self.app.overrideredirect(False)
+            self.app.attributes("-fullscreen", True)
+            self.app.state.is_fullscreen = True
+        else:
+
+            self.app.attributes("-fullscreen", False)
+            self.app.state.is_fullscreen = False
+
+    def toggle_sticky(self):
+        if not self.app.state.is_sticky:
+            self.app.state.is_sticky = True
+            self.app.attributes("-topmost", True)
+            self.app.geometry('85x40')
+            self.app.overrideredirect(True)
+            # Adjust timer display font and position
+            self.app.timer_display.config(font=("Helvetica", 20))
+            self.app.timer_display.grid(row=0, column=0, padx=5)
+        else:
+            self.app.state.is_sticky = False
+            self.app.attributes("-topmost", False)
+            self.app.geometry(self.app.state.geometry)
+            self.app.overrideredirect(False)
+            # Restore timer display font and position
+            self.app.timer_display.config(font=("Helvetica", 40))
+            self.app.timer_display.grid(row=0, column=1, padx=5)
+
+
+class ControlButtons(tk.Frame):
+    def __init__(self, master, command_handler: CommandHandler, **kwargs):
+        super().__init__(master, **kwargs)
+        self.command_handler = command_handler
+        self.start_button = ttk.Button(self, text="Start", command=self.command_handler.start_timer)
+        self.start_button.grid(row=0, column=0, padx=5, pady=5)
+
+        self.pause_button = ttk.Button(self, text="Pause", state="disabled", command=self.command_handler.pause_timer)
+        self.pause_button.grid(row=0, column=1, padx=5, pady=5)
+
+        self.reset_button = ttk.Button(self, text="Reset", state="disabled", command=self.command_handler.reset_timer)
+        self.reset_button.grid(row=1, column=0, padx=5, pady=5)
+
+        self.full_screen_button = ttk.Button(self, text="Full Screen", command=self.command_handler.toggle_fullscreen)
+        self.full_screen_button.grid(row=1, column=1, padx=5, pady=5)
+
+        self.sticky_button = ttk.Button(self, text="Sticky", command=self.command_handler.toggle_sticky)
+        self.sticky_button.grid(row=2, column=1, padx=5, pady=5)
+
+        self.grid(row=2, column=1, padx=5, pady=5)
 
 
 def main():
-    root = tk.Tk()
-    PomodoroTimer(root)
-    root.mainloop()
+    state = AppState()
+    app = App(state)
+    app.mainloop()
 
 
 if __name__ == "__main__":
